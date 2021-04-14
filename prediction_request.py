20c9d14ebe4d8d51fbe2850325ba3be69481463d
Taeo Kim <kweras95@gmail.com>
2021-04-14 16:29:49 +0900

    Add a comment for original prediction request and comment out print for debugging

--- prediction_request.py
+++ prediction_request.py
@@ -2,22 +2,20 @@ import json
 import requests
 import base64
 
-data = {}
+
 with open('./images/gf.jpg', mode='rb') as file:
     img = file.read()
 data = {"inputs": [{"b64": base64.encodebytes(img).decode("utf-8")}]}
 
 # Making the request
 json_response = requests.post("http://localhost:8501/v1/models/inception_v3/versions/2:predict", data=json.dumps(data))
-print(json_response.request)
-print(json_response.content)
+# print(json_response.request)
+# print(json_response.content)
 
 predictions = json.loads(json_response.text)['outputs']
 print("Predictions: ", predictions)
 
-# And returns:
-# b'{\n    "outputs": [\n        "giant panda"\n    ]\n}'
-
+# This is for original prediction request
 '''
 data = json.dumps({"signature_name": "serving_default", "instances": input_image.tolist()})
 headers = {"content-type": "application/json"}
